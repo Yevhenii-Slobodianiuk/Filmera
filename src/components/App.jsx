96a157b0129375e6ac4e0056878122997d8f12c0
Yevhenii <evgeniy.slobod9@gmail.com>
2024-04-24 15:30:36 +0300

    created basic routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
 	return (
-		<div>App</div>
+		<div>
+			<CssBaseline />
+			<main>
+				<Routes>
+					<Route path="/" element={<h1>Movies</h1>} />
+					<Route path="movie/:id" element={<h1>Movie Information</h1>} />
+					<Route path="actor/:id" element={<h1>Actor</h1>} />
+					<Route path="profile/:id" element={<h1>Profile</h1>} />
+				</Routes>
+			</main>
+		</div>
 	)
 }
 
