38c7a2e2f4070adf5e8256e01d01a6cc5f78a0b4
Yevhenii <evgeniy.slobod9@gmail.com>
2024-04-25 16:18:47 +0300

    created services for requests/added store

---
+++ src/app/store.js
@@ -0,0 +1,20 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import { tmdbApi } from "../services/TMDB";
+
+const store = configureStore({
+	reducer: {
+		[tmdbApi.reducerPath]: tmdbApi.reducer
+	},
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(tmdbApi.middleware),
+})
+
+
+export default store;
+
+// export default configureStore({
+// 	reducer: {
+// 		[tmdbApi.reducerPath]: tmdbApi.reducer,
+// 		middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(tmdbApi.middleware),
+// 	}
+// })
--- src/index.js
+++ src/index.js
@@ -2,19 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material';
+import { Provider } from 'react-redux';
 
 import App from './components/App';
+import store from './app/store';
 
 const theme = createTheme({});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<ThemeProvider theme={theme}>
-			<Router>
-				<App />
-			</Router>
-		</ThemeProvider>
+		<Provider store={store}>
+			<ThemeProvider theme={theme}>
+				<Router>
+					<App />
+				</Router>
+			</ThemeProvider>
+		</Provider>
 	</React.StrictMode>
 );
 
